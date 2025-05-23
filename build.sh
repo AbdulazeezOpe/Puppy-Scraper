8f6dccbf1f70d66f91cc34b4148dc75178206845
Abdulazeez Ibrahim <87902367+AbdulazeezOpe@users.noreply.github.com>
2025-05-23 07:30:29 +0100

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,9 +1,13 @@
 #!/usr/bin/env bash
-# install chrome
+
 mkdir -p ~/.chrome
-wget https://storage.googleapis.com/chrome-for-testing-public/116.0.5845.188/linux64/chrome-linux64.zip
+
+# Retry download up to 3 times
+curl --retry 3 -O https://storage.googleapis.com/chrome-for-testing-public/123.0.6312.86/linux64/chrome-linux64.zip
+
+# Extract
 unzip chrome-linux64.zip
 mv chrome-linux64 ~/.chrome/chrome
 
-# expose binary location for Render
+# Expose for app
 echo "GOOGLE_CHROME_BIN=$HOME/.chrome/chrome/chrome" >> $RENDER_ENV_FILE
