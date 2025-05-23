9d714560d517a070ea6aa8be651ed738dec24caa
Abdulazeez Ibrahim <87902367+AbdulazeezOpe@users.noreply.github.com>
2025-05-23 07:43:17 +0100

    Update build.sh

--- build.sh
+++ build.sh
@@ -2,12 +2,11 @@
 
 mkdir -p ~/.chrome
 
-# Retry download up to 3 times
+# Download & extract Chrome
 curl --retry 3 -O https://storage.googleapis.com/chrome-for-testing-public/123.0.6312.86/linux64/chrome-linux64.zip
-
-# Extract
-unzip chrome-linux64.zip
+unzip -o chrome-linux64.zip  # overwrite if already exists
+rm -rf ~/.chrome/chrome
 mv chrome-linux64 ~/.chrome/chrome
 
-# Expose for app
-echo "GOOGLE_CHROME_BIN=$HOME/.chrome/chrome/chrome" >> $RENDER_ENV_FILE
+# Write binary path to profile
+echo 'export GOOGLE_CHROME_BIN=$HOME/.chrome/chrome/chrome' >> ~/.profile
