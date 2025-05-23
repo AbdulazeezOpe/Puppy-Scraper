2201aa120b159d15c200c8ea31c43d24e19a2b7c
Abdulazeez Ibrahim <87902367+AbdulazeezOpe@users.noreply.github.com>
2025-05-23 09:10:04 +0100

    Update puppy_scraper_app.py

--- puppy_scraper_app.py
+++ puppy_scraper_app.py
@@ -17,14 +17,16 @@ def start_driver():
      # ✅ Required for headless on Render
     
     
-    options.binary_location = "/opt/render/project/.chrome/chrome/chrome"
-
-    
-    options.add_argument("--headless=new")
+    #options.binary_location = "/opt/render/project/.chrome/chrome/chrome"
+    options.add_argument('--ignore-certificate-errors')
+    options.add_argument('--ignore-ssl-errors')
+    options.add_argument('--ignore-certificate-errors')
+    options.add_argument('--ignore-ssl-errors')
+    options.add_argument("--headless")
     options.add_argument("--disable-dev-shm-usage")
     options.add_argument("--no-sandbox")
     options.add_argument("--disable-gpu")
-    options.add_argument("--window-size=1920,1080")
+    #options.add_argument("--window-size=1920,1080")
 
     options.add_argument('--disable-blink-features=AutomationControlled')
     options.add_argument('--user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.5845.188 Safari/537.36')
